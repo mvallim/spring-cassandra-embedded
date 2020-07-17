add28e989e1448e0bb06ef36fea6a60c12d97f88
Marcos Tischer Vallim <tischer@gmail.com>
2020-07-17 17:27:33 -0300

    first commit

---
+++ src/main/java/br/com/itau/spring/cassandra/embedded/Application.java
@@ -0,0 +1,15 @@
+package br.com.itau.spring.cassandra.embedded;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackageClasses = Application.class)
+public class Application {
+
+  public static void main(final String[] args) throws IOException {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
---
+++ src/main/java/br/com/itau/spring/cassandra/embedded/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package br.com.itau.spring.cassandra.embedded.config;
+
+import java.io.IOException;
+
+import org.apache.cassandra.service.CassandraDaemon;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class ApplicationConfig {
+
+  @Bean
+  public CassandraDaemon embeddedCassandraService() throws IOException {
+
+    final CassandraDaemon cassandraDaemon = CassandraDaemon.getInstanceForTesting();
+
+    cassandraDaemon.applyConfig();
+    cassandraDaemon.init(null);
+    cassandraDaemon.start();
+
+    return cassandraDaemon;
+
+  }
+
+}
