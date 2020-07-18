a0507f46758b2e051501fdbb2b8e8d45f2d64d14
Marcos Tischer Vallim <tischer@gmail.com>
2020-07-18 01:32:55 -0300

    Remove spring-boot-starter-web

--- src/main/java/br/com/spring/embedded/cassandra/Application.java
+++ src/main/java/br/com/spring/embedded/cassandra/Application.java
@@ -1,13 +1,24 @@
 package br.com.spring.embedded.cassandra;
 
+import org.apache.cassandra.service.CassandraDaemon;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackageClasses = Application.class)
-public class Application {
+public class Application implements CommandLineRunner {
 
   public static void main(final String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Override
+  public void run(final String... args) throws Exception {
+    final CassandraDaemon cassandraDaemon = new CassandraDaemon();
+
+    cassandraDaemon.applyConfig();
+    cassandraDaemon.init(null);
+    cassandraDaemon.start();
   }
 
 }
